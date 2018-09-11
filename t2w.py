d7f59b0c6a66f63f5f1da463903bfcd7edd0890c
Sean Taylor <sean.r.taylor.1912@gmail.com>
2018-09-12 10:54:02 +1200

    more updats to t2w.py

--- t2w.py
+++ t2w.py
@@ -3,7 +3,7 @@ def outmp3(text, fname="output"):
     f= open("tmp.tts","w+")
     f.write(text)
     f.close()
-    os.system("~/mimic -f tmp.tts -o tmp.wav")
+    os.system("mimic -f tmp.tts -o tmp.wav")
     os.system("ffmpeg -i tmp.wav -codec:a libmp3lame -qscale:a 2 {}.mp3".format(fname))
     os.system("rm tmp.tts")
     os.system("rm tmp.wav")
